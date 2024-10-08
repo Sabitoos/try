27f4f4dffc8693fd6a0e74cd17d5510d6f138ae3
Your Name <you@example.com>
2024-10-08 14:42:45 +0300

    ПЗ-4
    Берлизев, Апухтин научились определять формы

--- main/views.py
+++ main/views.py
@@ -5,10 +5,14 @@ from django.shortcuts import render
 from .forms import UserForm
 
 def index(request):
- userform = UserForm()
- return render(request, "main/index.html",
- {"form": userform})
-
+ if request.method == "POST":
+    name = request.POST.get("name") # получить значения поля Имя
+    age = request.POST.get("age") # значения поля Возраст
+    output = "<h2>Пользователь</h2><h3>Имя - {0}, Возраст – {1}</h3>".format(name, age)
+    return HttpResponse(output)
+ else:
+    userform = UserForm()
+    return render(request, "firstapp/index.html", {"form": userform})
 
 def products(request, productid = 1):
  output = "<h2>Продукт № {0}</h2>".format(productid)
